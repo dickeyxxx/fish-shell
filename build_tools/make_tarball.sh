dickeyxxx/fish-shell

commit 5023ade7aca320d5d7ebc031cb93f751734d29e3

tarball generation should build configure script
For the next release, this means that `autoconf` is no longer required
if building from the tarball. The website and documentation should be
updated accordingly at that time.

build_tools/make_tarball.sh
@@ -32,7 +32,8 @@ rm -f "$path" "$path".gz
 # git starts the archive
 git archive --format=tar --prefix="$prefix"/ master > "$path"
 
-# tarball out the documentation
+# tarball out the documentation, generate a configure script and version file
+autoconf
 make user_doc
 make share/man
 echo $VERSION > version
@@ -42,6 +43,7 @@ ln -s "$wd" "$prefix"
 gnutar --append --file="$path" "$prefix"/user_doc/html
 gnutar --append --file="$path" "$prefix"/share/man
 gnutar --append --file="$path" "$prefix"/version
+gnutar --append --file="$path" "$prefix"/configure
 rm -f "$prefix"/version
 rm -f "$prefix"
 

commit 01e19ae583bd0da22183a803fb2fb2d8d678396e

tarball generation: include config.h.in, set mode and ownership
Include config.h.in as well as configure.

Also sets correct owner, group and mode for all appended files.

Update the mtime of all appended files so that configure and config.h.in
are always newer than configure.ac.

(Fixes many problems introduced by 5023ade7, and makes the commit
message actually true.)

build_tools/make_tarball.sh
@@ -33,17 +33,18 @@ rm -f "$path" "$path".gz
 git archive --format=tar --prefix="$prefix"/ master > "$path"
 
 # tarball out the documentation, generate a configure script and version file
-autoconf
-make user_doc
-make share/man
+autoreconf
+./configure --with-doxygen
+make user_doc share/man
 echo $VERSION > version
 cd /tmp
 rm -f "$prefix"
 ln -s "$wd" "$prefix"
-gnutar --append --file="$path" "$prefix"/user_doc/html
-gnutar --append --file="$path" "$prefix"/share/man
-gnutar --append --file="$path" "$prefix"/version
-gnutar --append --file="$path" "$prefix"/configure
+TAR_APPEND="gnutar --append --file=$path --mtime=now --owner=root --group=root --mode=g+w,a+rX"
+$TAR_APPEND --no-recursion "$prefix"/user_doc
+$TAR_APPEND "$prefix"/user_doc/html "$prefix"/share/man
+$TAR_APPEND "$prefix"/version
+$TAR_APPEND "$prefix"/configure "$prefix"/config.h.in
 rm -f "$prefix"/version
 rm -f "$prefix"
 